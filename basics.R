d237d0f31b7e2df275a108fd34d85efe9ef76e4e
StaceyShe <101217444+StaceyShe@users.noreply.github.com>
2022-03-08 18:50:52 -0800

    Add files via upload

---
+++ basics.R
@@ -0,0 +1,21 @@
+# load packages --------
+
+library(tidyverse)
+library(here)
+library(skimr)
+
+
+# read in data ------
+
+beaches <- read.csv(here("data","sydneybeaches.csv"))
+
+
+# exploring the data -----
+view(beaches)
+dim(beaches)
+str(beaches)
+glimpse(beaches)
+head(beaches)
+tail(beaches)
+summary(beaches)
+skim(beaches)
